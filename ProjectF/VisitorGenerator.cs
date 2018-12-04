8a0712f8d8a9337a5a2f1f48c491e2170232740f
Egor Ivkov <e.o.ivkov@gmail.com>
2018-12-04 18:53:08 +0300

    function pass to another variable fix

--- ProjectF/VisitorGenerator.cs
+++ ProjectF/VisitorGenerator.cs
@@ -35,6 +35,7 @@ namespace ProjectF
         private Dictionary<string, string> varFunction = new Dictionary<string, string>();
         private Stack<string> initializers = new Stack<string>();
         private Stack<string> listVarNames = new Stack<string>();
+        private Stack<string> funcVarNames = new Stack<string>();
         private int varCount = 0;
 
         public override string VisitProgram([NotNull] ProjectFParser.ProgramContext context)
@@ -100,6 +101,7 @@ namespace ProjectF
             {
                 type = "void*";
                 ftype = FType.Function;
+                funcVarNames.Push(name);
             }
             if (type[0] == '(' && type[type.Length-1] == ')')
             {
@@ -110,7 +112,8 @@ namespace ProjectF
             }
             _symbolTable.Add(name, ftype);
             var expression = VisitExpression(context.expression());
-            varFunction.Add(name, expression.Substring(1));
+            if(!varFunction.Keys.Contains(name))
+                varFunction.Add(name, expression.Substring(1));
             var result = type + " " + outName + " = " + expression + ";\r\n";
             if(ftype == FType.List)
             {
@@ -204,12 +207,18 @@ namespace ProjectF
                     case FType.List:
                         result = "*(("+_listTable[id]+"*)l_get(" + id + "," + VisitExpression(context.tail()[0].expression()) + "));";
                         break;
-                    //Tuples and Maps
-                    case FType.Function:
-                        var fname = varFunction[id];
-                        result = "(" + functionCast[fname] + id + ")(" + VisitExpressions(context.tail()[0].expressions()) +");";
+                    //TODO: Tuples and Maps
+                    case FType.Function: 
+                        if (context.tail().Length > 0)
+                        { //func call
+                            var fname = varFunction[id];
+                            result = "(" + functionCast[fname] + id + ")(" + VisitExpressions(context.tail()[0].expressions()) + ");";
+                         } else
+                        {
+                            var fname = varFunction[id];
+                            varFunction.Add(funcVarNames.Pop(), fname);
+                        }
                         break;
-                    //func call
                     case FType.SystemFucntion:
                         var exType = DetectExpressionType(context.tail()[0].expressions().expression()[0]);
                         switch (exType)
@@ -262,11 +271,14 @@ namespace ProjectF
         public override string VisitExpressions([NotNull] ProjectFParser.ExpressionsContext context)
         {
             var expressions = "";
-            foreach(var expr in context.expression())
-            {
-                expressions += VisitExpression(expr) + ",";
+            if (context?.expression() != null) {
+                foreach (var expr in context.expression())
+                {
+                    expressions += VisitExpression(expr) + ",";
+                }
+                return expressions.Substring(0, expressions.Length - 1);
             }
-            return expressions.Substring(0, expressions.Length - 1);
+            return expressions;
         }
 
         private FType DetectExpressionType([NotNull] ProjectFParser.ExpressionContext context)
